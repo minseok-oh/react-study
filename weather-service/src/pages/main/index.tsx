597a4d561700a76f5649f909efc1325f26807a67
MinSeok Oh <minseok.dev@gmail.com>
2025-04-09 10:45:26 +0900

    feat: UI 상 현재 날씨 상태 출력 구현

--- weather-service/src/pages/main/index.tsx
+++ weather-service/src/pages/main/index.tsx
@@ -1,14 +1,51 @@
+import { useState } from 'react'
 import { getCurrentWeatherByFetch } from '../../apis/get-current-weather'
+import { Weather } from '../../types/weather'
 import { UserInputModal } from './user-input-modal'
 
 export const Main = () => {
+  const [weather, setWeather] = useState<Weather>()
+
   const getWeatherByUserPosition = async (nx: number, ny: number) => {
     const response = await getCurrentWeatherByFetch(nx, ny)
-    console.log(response)
+    const temperatures = response.filter(
+      (item: any) => item['category'] === 'T1H'
+    )
+    const rains = response.filter((item: any) => item['category'] === 'PTY')
+    const skies = response.filter((item: any) => item['category'] === 'SKY')
+
+    setWeather({
+      temp: temperatures[0]['fcstValue'],
+      rain: rains[0]['fcstValue'],
+      sky: skies[0]['fcstValue'],
+    })
+  }
+
+  const convertRainToKorean = (rain: string | undefined) => {
+    if (rain == '0') return '없음'
+    if (rain == '1') return '비'
+    if (rain == '2') return '비/눈'
+    if (rain == '3') return '눈'
+    if (rain == '5') return '빗방울'
+    if (rain == '6') return '빗방울눈날림'
+    if (rain == '7') return '눈날림'
+    return ''
+  }
+
+  const convertSkyToKorean = (sky: string | undefined) => {
+    if (sky == '1') return '맑음'
+    if (sky == '3') return '구름 많음'
+    if (sky == '4') return '흐림'
+    return ''
   }
 
   return (
     <div>
+      <div>
+        <p>온도: {weather?.temp}</p>
+        <p>하늘상태: {convertSkyToKorean(weather?.sky)}</p>
+        <p>강수상태: {convertRainToKorean(weather?.rain)}</p>
+      </div>
       <UserInputModal getWeatherByUserPosition={getWeatherByUserPosition} />
     </div>
   )
